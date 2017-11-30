1fd5ec8b4a11e72997489bb4d4dd5d72d0a5f612
Richard Hanten <richard.hanten@uni-tuebingen.de>
2017-11-30 14:59:38 +0100

    added 2d bresenham

---
+++ cslibs_math_2d/include/cslibs_math_2d/algorithms/amantidis.hpp
@@ -0,0 +1,12 @@
+#ifndef CSLIBS_MATH_2D_AMANTIDIS_HPP
+#define CSLIBS_MATH_2D_AMANTIDIS_HPP
+
+namespace cslibs_math_2d {
+namespace algrotihms {
+
+
+
+}
+}
+
+#endif // CSLIBS_MATH_2D_AMANTIDIS_HPP
---
+++ cslibs_math_3d/include/cslibs_math_3d/algorithms/amantidis.hpp
@@ -0,0 +1,12 @@
+#ifndef CSLIBS_MATH_3D_AMANTIDIS_HPP
+#define CSLIBS_MATH_3D_AMANTIDIS_HPP
+
+namespace cslibs_math_3d {
+namespace algrotihms {
+
+
+
+}
+}
+
+#endif // CSLIBS_MATH_3D_AMANTIDIS_HPP
