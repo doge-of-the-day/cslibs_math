105a8164e54e2ca206ab69ac12da4a2d254747ce
Richard Hanten <richard.hanten@uni-tuebingen.de>
2017-12-01 16:40:51 +0100

    ray tracing

--- cslibs_math/include/cslibs_math/common/array.hpp
+++ cslibs_math/include/cslibs_math/common/array.hpp
@@ -80,8 +80,7 @@ std::ostream & operator << (std::ostream &__out, const std::array<_Tp, _Nm> &__a
     return __out;
 }
 
-namespace cslibs_math {
-namespace common {
+namespace std {
 template<typename _Tp, std::size_t _Nm>
 std::array<_Tp, _Nm> min(const std::array<_Tp, _Nm> &__one,
                          const std::array<_Tp, _Nm> &__two)
@@ -101,7 +100,34 @@ std::array<_Tp, _Nm> max(const std::array<_Tp, _Nm> &__one,
         arr[i] = std::max(__one[i], __two[i]);
     return arr;
 }
+template<typename _Tp, std::size_t _Nm>
+std::array<_Tp, _Nm> abs(const std::array<_Tp, _Nm> &__one)
+{
+    std::array<_Tp, _Nm> arr;
+    for(std::size_t i = 0 ; i < _Nm ; ++i)
+        arr[i] = std::abs(__one[i]);
+    return arr;
+}
+template<typename _Tp, std::size_t _Nm>
+std::array<int, _Nm> compare (const std::array<_Tp, _Nm> &__one,
+                              const std::array<_Tp, _Nm> &__two)
+{
+    std::array<_Tp, _Nm> arr;
+    for(std::size_t i = 0 ; i < _Nm ; ++i)
+        arr[i] = __one[i] < __two[i] ? 1 : -1;
+    return arr;
+}
+
+template<typename _Tp, std::size_t _Nm>
+std::array<int, _Nm> compare (const std::array<_Tp, _Nm> &__one,
+                              const _Tp s)
+{
+    std::array<_Tp, _Nm> arr = __one;
+    for(std::size_t i = 0 ; i < _Nm ; ++i)
+        arr[i] = __one[i] < s ? 1 : -1;
+    return arr;
 }
 }
+
 
 #endif // CSLIBS_MATH_ARRAY_HPP
--- cslibs_math_3d/include/cslibs_math_3d/algorithms/bresenham.hpp
+++ cslibs_math_3d/include/cslibs_math_3d/algorithms/bresenham.hpp
@@ -1,10 +1,184 @@
 #ifndef CSLIBS_MATH_3D_BRESENHAM_HPP
 #define CSLIBS_MATH_3D_BRESENHAM_HPP
 
+#include <memory>
+#include <cslibs_math/common/array.hpp>
+#include <cslibs_math_3d/linear/point.hpp>
+
 namespace cslibs_math_3d {
 namespace algrotihms {
 
+class Bresenham
+{
+public:
+    using Ptr           = std::shared_ptr<Bresenham>;
+    using index_t       = std::array<int, 3>;
+    using error_t       = std::array<int, 2>;
+    using point_t       = Point3d;
+    using interation_t  = Bresenham& (Bresenham::*)() ; // cslibs_utility::common::delegate<Bresenham &()>;
+    using done_t        = bool (Bresenham::*)();        //cslibs_utility::common::delegate<bool()>;
 
+    inline Bresenham() :
+        start_{{0,0,0}},
+        end_  {{0,0,0}},
+        index_{{0,0,0}},
+        delta_abs_{{0,0,0}},
+        iteration_(0)
+    {
+    }
+
+    inline explicit Bresenham(const point_t &p0,
+                              const point_t &p1,
+                              const double resolution):
+        Bresenham({{static_cast<int>(p0(0) / resolution), static_cast<int>(p0(1)), static_cast<int>(p0(2) / resolution)}},
+    {{static_cast<int>(p1(0) / resolution), static_cast<int>(p1(1)), static_cast<int>(p1(2) / resolution)}})
+    {
+    }
+
+    inline explicit Bresenham(const index_t     &start,
+                              const index_t     &end) :
+        start_(start),
+        end_(end),
+        index_(start_),
+        error_{{0,0}},
+        iteration_(0)
+    {
+        delta_             = end_ - start_;
+        delta_abs_         = std::abs(delta_);
+        delta_abs_times_2_ = delta_abs_ * 2;
+        step_              = std::compare(start_, end_);
+
+        if(delta_abs_[0] >= delta_abs_[1] && delta_abs_[0] >= delta_abs_[2]) {
+            iterate_ = &Bresenham::iterateDx;
+            done_    = &Bresenham::doneDx;
+            error_[0] = delta_abs_times_2_[1] - delta_abs_[0];
+            error_[1] = delta_abs_times_2_[2] - delta_abs_[0];
+        } else if(delta_abs_[1] >= delta_abs_[0] && delta_abs_[1] >= delta_abs_[2]) {
+            iterate_ = &Bresenham::iterateDy;
+            done_    = &Bresenham::doneDy;
+            error_[0] = delta_abs_times_2_[0] - delta_abs_[1];
+            error_[1] = delta_abs_times_2_[2] - delta_abs_[1];
+        } else {
+            iterate_ = &Bresenham::iterateDz;
+            done_    = &Bresenham::doneDz;
+            error_[0] = delta_abs_times_2_[1] - delta_abs_[2];
+            error_[1] = delta_abs_times_2_[0] - delta_abs_[2];
+        }
+    }
+
+    inline virtual ~Bresenham()
+    {
+    }
+
+    inline int x() const
+    {
+        return index_[0];
+    }
+
+    inline int y() const
+    {
+        return index_[1];
+    }
+
+    inline int z() const
+    {
+        return index_[2];
+    }
+
+
+    inline Bresenham& operator++()
+    {
+        return (this->*done_)() ? *this : (this->*iterate_)();
+    }
+
+    inline int length2() const
+    {
+        auto sq = [](const int d) { return d*d;};
+        return sq(index_[0] - end_[0]) + sq(index_[1] - end_[1] + sq(index_[2] - end_[2]));
+    }
+
+    inline bool done() const
+    {
+        return (this->*done_)();
+    }
+
+private:
+    inline Bresenham &iterateDx()
+    {
+        if (error_[0] > 0) {
+            index_[1] += step_[1];
+            error_[0] -= delta_abs_times_2_[0];
+        }
+        if (error_[1] > 0) {
+            index_[2] += step_[2];
+            error_[1] -= delta_abs_times_2_[0];
+        }
+        error_[0] += delta_abs_times_2_[1];
+        error_[1] += delta_abs_times_2_[2];
+        index_[0] += step_[0];
+        ++iteration_;
+        return *this;
+    }
+    inline bool doneDx() const
+    {
+        return iteration_ >= delta_abs_[0];
+    }
+
+    inline Bresenham &iterateDy()
+    {
+        if (error_[0] > 0) {
+            index_[0] += step_[0];
+            error_[0] -= delta_abs_times_2_[1];
+        }
+        if (error_[1] > 0) {
+            index_[2] += step_[2];
+            error_[1] -= delta_abs_times_2_[1];
+        }
+        error_[0] += delta_abs_times_2_[0];
+        error_[1] += delta_abs_times_2_[2];
+        index_[1] += step_[1];
+        ++iteration_;
+        return *this;
+    }
+    inline bool doneDy() const
+    {
+        return iteration_ >= delta_abs_[1];
+    }
+
+    inline Bresenham &iterateDz()
+    {
+        if (error_[0] > 0) {
+            index_[1] += step_[1];
+            error_[0] -= delta_abs_times_2_[2];
+        }
+        if (error_[1] > 0) {
+            index_[0] += step_[0];
+            error_[1] -= delta_abs_times_2_[2];
+        }
+        error_[0] += delta_abs_times_2_[1];
+        error_[1] += delta_abs_times_2_[0];
+        index_[2] += step_[2];
+        ++iteration_;
+        return *this;
+    }
+    inline bool doneDz() const
+    {
+        return iteration_ >= delta_abs_[2];
+    }
+
+    index_t         start_;
+    index_t         end_;
+    index_t         index_;
+    index_t         step_;
+    index_t         delta_;
+    index_t         delta_abs_;
+    index_t         delta_abs_times_2_;
+    error_t         error_;
+    Bresenham&     (Bresenham::*iterate_)() ;
+    bool           (Bresenham::*done_)() const;
+    int             iteration_;
+
+};
 
 }
 }
