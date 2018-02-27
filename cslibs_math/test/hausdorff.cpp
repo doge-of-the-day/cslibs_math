8930342e830d2b65760310d711f2db1bc370ef8b
Cornelia Schulz <cornelia.schulz@uni-tuebingen.de>
2018-02-27 12:09:20 +0100

    hausdorff test

--- cslibs_math/test/hausdorff.cpp
+++ cslibs_math/test/hausdorff.cpp
@@ -1,12 +1,12 @@
 #include <cslibs_math/linear/hausdorff.hpp>
 
 using point_t = cslibs_math::linear::Vector<double, 2>;
-using pointlcoud_t = cslibs_math::linear::Pointcloud<point_t>;
+using pointcloud_t = cslibs_math::linear::Pointcloud<point_t>;
 
 
 int main(int argc, char *argv[])
 {
-    pointlcoud_t pts;
+    pointcloud_t pts;
     point_t      p = point_t::random();
     for(std::size_t i = 0 ; i < 10 ; ++i) {
         pts.insert(point_t::random());
