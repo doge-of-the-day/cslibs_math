cb09c6f328c53366341441639a8d425b56ebc4ed
Adrian Zwiener <adrian.zwiener@uni-tuebingen.de>
2018-11-02 20:15:03 +0100

    Add conversion method

--- cslibs_math_ros/include/cslibs_math_ros/geometry_msgs/conversion_3d.hpp
+++ cslibs_math_ros/include/cslibs_math_ros/geometry_msgs/conversion_3d.hpp
@@ -1,4 +1,27 @@
 #ifndef CSLIBS_MATH_ROS_GEOMETRY_MSGS_CONVERSION_3D_HPP
 #define CSLIBS_MATH_ROS_GEOMETRY_MSGS_CONVERSION_3D_HPP
+#include <tf/tf.h>
+#include <geometry_msgs/Point.h>
+#include <cslibs_math_3d/linear/vector.hpp>
 
+namespace cslibs_math_ros {
+namespace geometry_msgs {
+namespace conversion_3d {
+inline cslibs_math_3d::Vector3d from(const ::geometry_msgs::Point &p)
+{
+    return cslibs_math_3d::Vector3d(p.x, p.y, p.z);
+}
+
+inline ::geometry_msgs::Point toPoint(const ::cslibs_math_3d::Vector3d &p)
+{
+    ::geometry_msgs::Point res;
+    res.x = p(0);
+    res.y = p(1);
+    res.z = p(2);
+    return res;
+}
+
+}
+}
+}
 #endif // CSLIBS_MATH_ROS_GEOMETRY_MSGS_CONVERSION_3D_HPP
