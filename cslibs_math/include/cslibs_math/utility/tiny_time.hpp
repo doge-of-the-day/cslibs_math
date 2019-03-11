145c11336f3ac5838aa7541b703fc8744004bf16
Cornelia Schulz <cornelia.schulz@uni-tuebingen.de>
2019-03-11 14:59:20 +0100

    minor

--- cslibs_math/include/cslibs_math/utility/tiny_time.hpp
+++ cslibs_math/include/cslibs_math/utility/tiny_time.hpp
@@ -26,43 +26,43 @@ inline time_t fromNanoseconds(const int64_t n)
     return time_t(std::chrono::nanoseconds(n));
 }
 
-template <typename T>
+template <typename T = double>
 inline T seconds(const time_t &time)
 {
     return std::chrono::duration_cast<std::chrono::nanoseconds>(time.time_since_epoch()).count() * 1e-9;
 }
 
-template <typename T>
+template <typename T = double>
 inline T milliseconds(const time_t &time)
 {
     return std::chrono::duration_cast<std::chrono::nanoseconds>(time.time_since_epoch()).count() * 1e-6;
 }
 
-template <typename T>
+template <typename T = double>
 inline T microseconds(const time_t &time)
 {
     return std::chrono::duration_cast<std::chrono::nanoseconds>(time.time_since_epoch()).count() * 1e-3;
 }
 
-template <typename T>
+template <typename T = double>
 inline T nanoseconds(const time_t &time)
 {
     return std::chrono::duration_cast<std::chrono::nanoseconds>(time.time_since_epoch()).count();
 }
 
-template <typename T>
+template <typename T = double>
 inline T seconds(const duration_t &duration)
 {
     return std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count() * 1e-9;
 }
 
-template <typename T>
+template <typename T = double>
 inline T milliseconds(const duration_t &duration)
 {
     return std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count() * 1e-6;
 }
 
-template <typename T>
+template <typename T = double>
 inline T microseconds(const duration_t &duration)
 {
     return std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count() * 1e-3;
