7e13491a21c878b160eb0923ad94585b6725016d
Richard Hanten <richard.hanten@gmail.com>
2020-04-21 13:33:16 +0200

    cleaning up cmake in cslibs_math

--- cslibs_math/cmake/cslibs_math_add_unit_test_gtest.cmake
+++ cslibs_math/cmake/cslibs_math_add_unit_test_gtest.cmake
@@ -1,34 +1,51 @@
-# ${PROJECT_NAME}_add_unit_test_gtest(<NAME_OF_THE_TEST> [<SRCS> ...] [<LIBS> ...]) 
+# ${PROJECT_NAME}_add_unit_test_gtest(<NAME_OF_THE_TEST> [<SRCS> ...] [<LIBS> ...])
 function(${PROJECT_NAME}_add_unit_test_gtest)
     cmake_parse_arguments(unit_test
-        ""          # list of names of the boolean arguments (only defined ones will be true)
-        ""          # list of names of mono-valued arguments
-        "SRCS;LIBS" # list of names of multi-valued arguments (output variables are lists)
-        ${ARGN}     # arguments of the function to parse, here we take the all original ones
+        ""                                          # list of names of the boolean arguments (only defined ones will be true)
+        ""                                          # list of names of mono-valued arguments
+        "INCLUDE_DIRS;SOURCE_FILES;LINK_LIBRARIES"  # list of names of multi-valued arguments (output variables are lists)
+        ${ARGN}                                     # arguments of the function to parse, here we take the all original ones
     )
 
     if(${catkin_FOUND})
         set(unit_test_NAME ${ARGV0})
         find_package(Boost REQUIRED COMPONENTS system)
-        add_definitions(-pthread)
+
         catkin_add_gtest(${unit_test_NAME}
-            ${unit_test_SRCS}
+            ${unit_test_SOURCE_FILES}
         )
+
+        target_include_directories(${unit_test_NAME}
+            PRIVATE
+                ${unit_test_INCLUDE_DIRS}
+                ${Boost_INCLUDE_DIRS}
+        )
+
         target_link_libraries(${unit_test_NAME}
-            ${unit_test_LIBS}
-            ${GTEST_LIBRARIES}
-            ${Boost_LIBRARIES}
-            -lpthread
+            #PRIVATE
+                ${unit_test_LINK_LIBRARIES}
+                ${GTEST_LIBRARIES}
+                ${Boost_LIBRARIES}
+                -pthread
         )
     else()
         set(unit_test_NAME ${PROJECT_NAME}_${ARGV0})
         enable_testing()
+
         add_executable(${unit_test_NAME}
-            ${unit_test_SRCS}
+            ${unit_test_SOURCE_FILES}
         )
+
+        target_include_directories(${unit_test_NAME}
+            PRIVATE
+                ${unit_test_INCLUDE_DIRS}
+        )
+
         target_link_libraries(${unit_test_NAME}
-            ${unit_test_LIBS}
-            ${GTEST_LIBRARIES}
+            PRIVATE
+                ${unit_test_LINK_LIBRARIES}
+                ${GTEST_LIBRARIES}
+                -pthread
         )
         add_test(AllTestsIn${unit_test_NAME} ${unit_test_NAME})
     endif()
--- cslibs_math/cmake/cslibs_math_add_unit_test_ros.cmake
+++ cslibs_math/cmake/cslibs_math_add_unit_test_ros.cmake
@@ -1,27 +1,33 @@
 # ${PROJECT_NAME}_add_unit_test_gtest(<NAME_OF_THE_TEST> [<SRCS> ...] [<LIBS> ...])
 function(${PROJECT_NAME}_add_unit_test_ros)
     cmake_parse_arguments(unit_test
-        ""          # list of names of the boolean arguments (only defined ones will be true)
-        "LAUNCH"    # list of names of mono-valued arguments
-        "SRCS;LIBS" # list of names of multi-valued arguments (output variables are lists)
-        ${ARGN}     # arguments of the function to parse, here we take the all original ones
+        ""                                         # list of names of the boolean arguments (only defined ones will be true)
+        "LAUNCH_FILES"                             # list of names of mono-valued arguments
+        "INCLUDE_DIRS;SOURCE_FILES;LINK_LIBRARIES" # list of names of multi-valued arguments (output variables are lists)
+        ${ARGN}                                    # arguments of the function to parse, here we take the all original ones
     )
 
     find_package(rostest QUIET)
     find_package(roscpp QUIET)
     if(${rostest_FOUND})
-        include_directories(${rostest_INCLUDE_DIRS}
-                            ${roscpp_INCLUDE_DIRS})
         set(unit_test_NAME ${ARGV0})
         add_rostest_gtest(${unit_test_NAME}
-                          ${unit_test_LAUNCH}
-                          ${unit_test_SRCS})
+                          ${unit_test_LAUNCH_FILES}
+                          ${unit_test_SOURCE_FILES})
 
+        target_include_directories(${unit_test_NAME}
+            PRIVATE
+                ${unit_test_INCLUDE_DIRS}
+                ${rostest_INCLUDE_DIRS}
+                ${roscpp_INCLUDE_DIRS}
+        )
         target_link_libraries(${unit_test_NAME}
-              ${unit_test_LIBS}
-              ${rostest_LIBRARIES}
-              ${roscpp_LIBRARIES}
-              ${GTEST_LIBRARIES}
+            #PRIVATE
+                ${unit_test_LIBS}
+                ${unit_test_LINK_LIBRARIES}
+                ${rostest_LIBRARIES}
+                ${roscpp_LIBRARIES}
+                ${GTEST_LIBRARIES}
         )
     endif()
 endfunction()
