fe1d36948779762db8dbb854874aa53a54761869
Richard Hanten <richard.hanten@gmail.com>
2020-05-11 13:01:25 +0200

    power

--- cslibs_math/include/cslibs_math/approx/exp.hpp
+++ cslibs_math/include/cslibs_math/approx/exp.hpp
@@ -1,11 +1,16 @@
 #ifndef CSLIBS_MATH_EXP_HPP
 #define CSLIBS_MATH_EXP_HPP
 
-#include <limits>
+#include <cslibs_math/common/pow.hpp>
 
 namespace cslibs_math {
 namespace approx {
-
+template<unsigned int Accuracy_T, typename T = double>
+inline constexpr T exp(const T value)
+{
+    const auto x = T{1} + value / common::pow2<Accuracy_T, T>();
+    return common::pow<Accuracy_T + 1ul>(x);
+}
 }  // namespace common
 }  // namespace cslibs_math
 
