e49106412debef4c269252c696a4b6ed5e1744a7
Richard Hanten <richard.hanten@gmail.com>
2020-05-13 18:56:20 +0200

    fractional number representation

---
+++ cslibs_math/include/cslibs_math/approx/fractional.hpp
@@ -0,0 +1,61 @@
+#ifndef CSLIBS_MATH_FRACTIONAL_HPP
+#define CSLIBS_MATH_FRACTIONAL_HPP
+
+#include <cmath>
+#include <type_traits>
+#include <limits>
+
+namespace cslibs_math {
+namespace approx {
+
+template<typename T>
+class Fractional 
+{
+public:
+	static_assert(std::is_floating_point<T>::value, "Type must be floating point.");
+
+	Fractional() = default;
+	explicit Fractional(const T value) : fraction_{std::frexp(value, &exponent_)} {}
+	explicit Fractional(const T fraction, const int exponent) : exponent_{exponent}, fraction_{fraction}{}
+
+
+	inline T value() const 
+	{
+		return std::ldexp(fraction_, exponent_);
+	}
+
+	inline T fraction() const
+	{
+		return fraction_;
+	}
+
+	inline int exponent() const
+	{
+		return exponent_;
+	}
+
+private:
+	int exponent_{0};
+	T fraction_{0};
+
+};
+}  // namespace common
+}  // namespace cslibs_math
+
+template<typename T>
+inline cslibs_math::approx::Fractional<T> operator * (const cslibs_math::approx::Fractional<T> &a, const cslibs_math::approx::Fractional<T> &b)
+{
+	auto factor = cslibs_math::approx::Fractional<T>(a.fraction() * b.fraction());
+	auto exponent = a.exponent() + b.exponent() + factor.exponent();
+	return cslibs_math::approx::Fractional<T>(factor.fraction(), exponent);
+}
+
+template<typename T>
+inline cslibs_math::approx::Fractional<T> operator + (const cslibs_math::approx::Fractional<T> &a, const cslibs_math::approx::Fractional<T> &b)
+{
+	const auto exponent_diff = a.exponent() - b.exponent();
+ 	const auto scale = std::ldexp(1.0, exponent_diff);
+ 	return cslibs_math::approx::Fractional<T>(b.fraction() + scale * a.fraction(), b.exponent());
+}
+
+#endif  // CSLIBS_MATH_FRACTIONAL_HPP
---
+++ cslibs_math/test/test_fractional.cpp
@@ -0,0 +1,73 @@
+#include <gtest/gtest.h>
+
+#include <cslibs_math/approx/fractional.hpp>
+#include <cslibs_math/random/random.hpp>
+
+using Fractionald = cslibs_math::approx::Fractional<double>;
+
+using rng_t = cslibs_math::random::Uniform<double, 1>;
+const auto REPETITIONS = 10000;
+
+TEST(Test_cslibs_math, testFractionalDefault) {
+	Fractionald f;
+	EXPECT_EQ(0.0, f.exponent());
+	EXPECT_EQ(0.0, f.fraction());
+}
+
+TEST(Test_cslibs_math, testFractionalValue) {
+
+  {
+	  rng_t rng(0.0, 10000000.0);
+	  for (std::size_t i = 0; i < REPETITIONS; ++i) {
+	    const auto val = rng.get();
+	    int e;
+	    auto f = std::frexp(val, &e);
+
+	  	Fractionald fr(val);
+	  	EXPECT_EQ(e, fr.exponent());
+	  	EXPECT_EQ(f, fr.fraction());
+	  }
+  }
+  {
+	  rng_t rng(0.0, 0.5);
+	  for (std::size_t i = 0; i < REPETITIONS; ++i) {
+	    const auto f = rng.get();
+	    const auto v = std::ldexp(f, 4);
+
+	  	Fractionald fr(v);
+		EXPECT_EQ(v, fr.value());
+	  }
+	}
+}
+
+TEST(Test_cslibs_math, testMult) {
+  rng_t rng(0.0, 100.0);
+  for (std::size_t i = 0; i < REPETITIONS; ++i) {
+    const auto a = rng.get();
+    const auto b = rng.get();
+  	Fractionald fra(a);
+  	Fractionald frb(b);
+  	const auto f = fra * frb;
+
+	EXPECT_EQ(a * b, f.value());
+  }
+}
+
+TEST(Test_cslibs_math, testAdd) {
+  rng_t rng(0.0, 100.0);
+  for (std::size_t i = 0; i < REPETITIONS; ++i) {
+    const auto a = rng.get();
+    const auto b = rng.get();
+  	Fractionald fra(a);
+  	Fractionald frb(b);
+  	const auto f = fra + frb;
+
+	EXPECT_EQ(a + b, f.value());
+  }
+}
+
+
+int main(int argc, char *argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
